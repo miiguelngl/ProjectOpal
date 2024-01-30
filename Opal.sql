b95facc3befa9c29ae24a8a953084fd5a297ecb7
Alvaro Medina Momparler <alvaroboca87@gmail.com>
2024-01-30 13:04:33 +0100

    Comienzo de tienda.php e inserccion de datos en la base de datos

---
+++ Opal.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 30-01-2024 a las 13:03:26
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `Opal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Fotos`
+--
+-- Error leyendo la estructura de la tabla Opal.Fotos: #1142 - SHOW comando negado para usuario: &#039;&#039;@&#039;localhost&#039; para tabla `Opal`.`Fotos`
+-- Error leyendo datos de la tabla Opal.Fotos: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `Opal`.`Fotos`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Usuario`
+--
+-- Error leyendo la estructura de la tabla Opal.Usuario: #1142 - SHOW comando negado para usuario: &#039;&#039;@&#039;localhost&#039; para tabla `Opal`.`Usuario`
+-- Error leyendo datos de la tabla Opal.Usuario: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `Opal`.`Usuario`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Venta`
+--
+-- Error leyendo la estructura de la tabla Opal.Venta: #1142 - SHOW comando negado para usuario: &#039;&#039;@&#039;localhost&#039; para tabla `Opal`.`Venta`
+-- Error leyendo datos de la tabla Opal.Venta: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `Opal`.`Venta`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Zapatillas`
+--
+-- Error leyendo la estructura de la tabla Opal.Zapatillas: #1142 - SHOW comando negado para usuario: &#039;&#039;@&#039;localhost&#039; para tabla `Opal`.`Zapatillas`
+-- Error leyendo datos de la tabla Opal.Zapatillas: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `Opal`.`Zapatillas`&#039; en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
