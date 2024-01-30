ebdab32005ab6111d657d8fe599b37e53406ef0a
Alvaro Medina Momparler <alvaroboca87@gmail.com>
2024-01-30 08:10:29 +0100

    Cambio no subido anterior sesion

--- php/catalogo.php
+++ php/catalogo.php
@@ -1,21 +1,21 @@
 <?php
 include("conexion.php");
-$consulta = mysqli_query($conexion, "SELECT COUNT(*) AS total FROM `Zapatillas`");
+$consulta1 = mysqli_query($conexion, "SELECT COUNT(*) AS total FROM `Zapatillas`");
 
-if ($consulta) {
+if ($consulta1) {
     //Obtiene los resultados de la consulta como una array asociativo
-    $fila = mysqli_fetch_assoc($consulta);
+    $fila = mysqli_fetch_assoc($consulta1);
     // Obtener el valor de la columna 'total'
     $totalZapatillas = $fila['total'];
 
     for ($i = 0; $i < $totalZapatillas; $i++) {
-    echo '<h5 class=card-text px-2 mt-1></h5>'
+        $consulta2=mysqli_query($conexion, "SELECT COUNT(*) AS total FROM `Zapatillas`");
+    echo '<h5 class=card-text px-2 mt-1></h5>';
         
 
     }
 
-    // Liberar el conjunto de resultados
-    mysqli_free_result($consulta);
+
 }
 
 ?>
