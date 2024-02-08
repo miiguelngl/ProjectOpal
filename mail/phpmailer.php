558a2a06589260e14ef248c82fc94c6dc548826c
miiguelngl <04mangel@gmail.com>
2024-02-08 20:24:56 +0100

    Sistema de correo funcionando

---
+++ mail/phpmailer.php
@@ -0,0 +1,71 @@
+<?php
+//Carga de las clases necesarias
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+//Crear una instancia. Con true permitimos excepciones
+$mail = new PHPMailer(true);
+
+try {
+    //Valores dependientes del servidor que utilizamos
+    
+    $mail->isSMTP();                                           //Para usaar SMTP
+    $mail->Host       = 'smtp-mail.outlook.com';                     //Nuestro servidor SMTMP smtp.gmail.com en caso de usar gmail
+    $mail->SMTPAuth   = true;    
+    /* 
+    * SMTP username y password Poned los vuestros. La contraseña es la que nos generó GMAIL
+    */
+    $mail->Username   = 'opalservice@outlook.es';             
+    $mail->Password   = 'Yovoyahaceruncorral';    
+    /*
+    * Encriptación a usar ssl o tls, dependiendo cual usemos hay que utilizar uno u otro puerto
+    */            
+    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;   
+    // $mail->Port = "465";
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+    /**TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`                         
+     * $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+     * $mail->Port       = 587;  
+     */
+
+
+    /*
+    Receptores y remitente
+    */
+//Remitente
+    $mail->setFrom('opalservice@outlook.es', 'Opal');
+//Receptores. Podemos añadir más de uno. El segundo argumento es opcional, es el nombre
+    $mail->addAddress('04mangel@gmail.com', 'Miguel');     //Add a recipient
+    //$mail->addAddress('ejemplo@example.com'); 
+
+    //Copia
+    //$mail->addCC('cc@example.com');
+    //Copia Oculta
+    //$mail->addBCC('bcc@example.com');
+
+    //Archivos adjuntos
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Contenido
+    //Si enviamos HTML
+    $mail->isHTML(true);    
+    $mail->CharSet = "UTF8";    
+    //Asunto
+    $mail->Subject = 'Here is the subject';
+    //Conteido HTML
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    //Contenido alternativo en texto simple
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    //Enviar correo
+    $mail->send();
+    echo 'El mensaje se ha enviado con exito';
+} catch (Exception $e) {
+    echo "El mensaje no se ha enviado: {$mail->ErrorInfo}";
+    
+}
+?>
