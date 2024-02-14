8076ed625a6b97950cbe544d13089027e1e58525
Alvaro Medina Momparler <alvaroboca87@gmail.com>
2024-02-14 09:03:19 +0100

    404 css

--- 404.php
+++ 404.php
@@ -4,14 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="css/carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/productoCase.css">
+    <script defer src="js/enlaces.js"></script>
+    <script defer src="js/menuDesplegable.js"></script>
+    <title>Opal</title>
+    <style>
+        h2{
+            font-family: 'Exo 2', sans-serif;
+            font-weight: 700;
+            margin: 20px;
+        }
+    </style>
 </head>
 <body>
     <?php
         include './php/header.php'
     ?>
-    <h2>ERROR 404 PAGE NOT FOUND</h2>
-    <?php
-        include './php/footer.php'
-    ?>
+    <h2 class="text-center">ERROR 404 PAGE NOT FOUND</h2>
+
 </body>
 </html>
