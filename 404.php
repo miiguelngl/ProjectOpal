48627e3a49d2fca56499c17bf2886c1bb6602fae
Alvaro Medina Momparler <alvaroboca87@gmail.com>
2024-02-14 08:58:51 +0100

    404 page

---
+++ 404.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include './php/header.php'
+    ?>
+    <h2>ERROR 404 PAGE NOT FOUND</h2>
+    <?php
+        include './php/footer.php'
+    ?>
+</body>
+</html>
