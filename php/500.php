91d49a10221b7c179011fdf841b528133c680279
miiguelngl <04mangel@gmail.com>
2024-02-14 12:50:43 +0100

    Error 500

---
+++ php/500.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="css/carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/productoCase.css">
+    <script defer src="js/enlaces.js"></script>
+    <script defer src="js/menuDesplegable.js"></script>
+    /* Para directivas */
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link href="../css/carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/productoCase.css">
+    <script defer src="../js/enlaces.js"></script>
+    <script defer src="../js/menuDesplegable.js"></script>
+
+    <link rel="stylesheet" href="../../css/normalize.css">
+    <link href="../../css/carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/index.css">
+    <link rel="stylesheet" href="../../css/productoCase.css">
+    <script defer src="../../js/enlaces.js"></script>
+    <script defer src="../../js/menuDesplegable.js"></script>
+    <style>
+        h2{
+            font-family: 'Exo 2', sans-serif;
+            font-weight: 700;
+            margin: 20px;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        include './php/header.php'
+    ?>
+    <h2 class="text-center">ERROR 500</h2>
+
+</body>
+</html>
